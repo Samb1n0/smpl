b6dab7269645859f08cd23888d29b8f42b8e8697
Tom Harper <tom@stickman.co.uk>
2016-03-18 10:36:02 +0000

    Added different headings for squat bench and deadlift rules

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 
 $(document).ready(function(){
 
-$("#showperfrules").click(function(){
-    $("#perfrules").toggleClass("hidden");
+$("#showsquatrules").click(function(){
+    $("#squatrules").toggleClass("hidden");
 });
+
+$("#showbenchrules").click(function(){
+    $("#benchrules").toggleClass("hidden");
+});
+
+$("#showdeadliftrules").click(function(){
+    $("#deadliftrules").toggleClass("hidden");
+});
+
 $("#showequiprules").click(function(){
     $("#equiprules").toggleClass("hidden");
 });
+
 $("#showagerules").click(function(){
     $("#agerules").toggleClass("hidden");
 });
+
 $("#showbodyweightrules").click(function(){
     $("#bodyweightrules").toggleClass("hidden");
 });
+
 $("#showweighingrules").click(function(){
     $("#weighingrules").toggleClass("hidden");
 });
+
 });
